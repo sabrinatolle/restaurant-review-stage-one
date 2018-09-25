3ef5e74792ad1229fc90bf3ec04363270b873f37
Sabrina Tolle <sabrinatolle@outlook.com>
2018-09-25 02:04:06 -0400

    service worker

--- sw.js
+++ sw.js
@@ -55,7 +55,7 @@ self.addEventListener('activate', function (event){
 
 self.addEventListener('fetch', function (event){
     event.respondWith(
-        caches.match(event, request).then(function (response){
+        caches.match(event,request).then(function (response){
             return response || fetch(event.request);
         })
     );
